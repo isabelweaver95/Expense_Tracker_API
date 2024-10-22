0ee69a93f8f0c06d517d8b994f6a833799581db3
isabelweaver95 <izzyweav@gmail.com>
2024-10-21 23:19:22 -0600

    started to add Expense members. Added a one to many relationship in catagory.

--- ExpenseTracker/Catagory.cs
+++ ExpenseTracker/Catagory.cs
@@ -2,5 +2,8 @@ namespace ExpenseTracker.Models{
     public class Catagory{
         public int Id {get; set;}
         public string Name{get; set;}
+
+        //Establishing a one to many relationship with Expenses
+        public list<Expense> Expenses {get; set;}
     }
 }
--- ExpenseTracker/Expense.cs
+++ ExpenseTracker/Expense.cs
@@ -1,5 +1,7 @@
 namespace ExpenseTracker.Models{
     public class Expense{
-        
+        public int Id {get; set;}
+        public decimal Amount {get; set;}
+        public DateTime date {get; set;}
     }
 }
