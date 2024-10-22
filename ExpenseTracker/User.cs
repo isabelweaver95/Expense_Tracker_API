3f0b80e25eb189c751a7984d67dbbb66bea664de
isabelweaver95 <izzyweav@gmail.com>
2024-10-21 22:36:06 -0600

    Added mySQL and the user class

---
+++ ExpenseTracker/User.cs
@@ -0,0 +1,9 @@
+namespace ExpenseTracker.Models{
+    public class User{
+        private string Username {get; set;}
+        private string Password {get; set;}
+
+        private string Email {get; set;}
+
+    }
+}
