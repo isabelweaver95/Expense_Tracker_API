50879873de80510283c197f8a0b58790eff7067d
isabelweaver95 <izzyweav@gmail.com>
2024-10-22 22:14:18 -0600

    Added the hash for the password, and edited expense

--- ExpenseTracker/Expense.cs
+++ ExpenseTracker/Expense.cs
@@ -3,5 +3,9 @@ namespace ExpenseTracker.Models{
         public int Id {get; set;}
         public decimal Amount {get; set;}
         public DateTime date {get; set;}
+        public string Description {get; set;}
+        public int CatagoryID {get; set;}
+        public Catagory catagory {get; set;}
+
     }
 }
--- ExpenseTracker/User.cs
+++ ExpenseTracker/User.cs
@@ -1,9 +1,9 @@
 namespace ExpenseTracker.Models{
     public class User{
-        public string Username {get; set;}
-        public string Password {get; set;}
-
-        public string Email {get; set;}
         public int Id {get; set;}
+        public string Username {get; set;}
+        public string PasswordHash {get; set;}
+        public string Email {get; set;}
+        public string Salt {get; set;}
     }
 }
