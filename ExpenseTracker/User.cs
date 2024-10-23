5e6091398c438c838cef9f5590eef84c7c09967c
isabelweaver95 <izzyweav@gmail.com>
2024-10-22 22:28:03 -0600

    Added a SetPassword, HashPassword, GenerateSalt to have a secure password that will be stored.

--- ExpenseTracker/User.cs
+++ ExpenseTracker/User.cs
@@ -5,5 +5,25 @@ namespace ExpenseTracker.Models{
         public string PasswordHash {get; set;}
         public string Email {get; set;}
         public string Salt {get; set;}
+
+        //This is a method to store and hide the password
+        public void SetPassword(string password){
+            Salt = GenerateSalt();
+            PasswordHash = HashPassword(password)
+        }
+
+        private string HashPassword(string password){
+            using(var sha256 = sha256.Create()){
+                var combined = Encoding.UTF8.GetBytes(password + Salt);
+                return Convert.ToBase64String(sha256.ComputeHash(combined));
+            }
+        }
+
+        private string GenerateSalt(){
+            var rng = new RNGCryptoServiceProvider();
+            var saltBytes = new byte[16];
+            rng.GetBytes(saltBytes);
+            return Convert.ToBase64String(saltBytes);
+        }
     }
 }
