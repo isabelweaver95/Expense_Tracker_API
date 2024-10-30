b772239fcf7d9ffc2f2d94d1b1b6d2b060b7f2fb
isabelweaver95 <izzyweav@gmail.com>
2024-10-30 14:04:00 -0600

    Trying to figure out error

--- ExpenseTracker/Database/DatabaseHelper.cs
+++ ExpenseTracker/Database/DatabaseHelper.cs
@@ -1,5 +1,6 @@
 using ExpenseTracker.Models;
-using MySql.Data.MySqlClient;
+using MySqlConnector
+
 
 namespace ExpenseTracker.Database{
     public class DatabaseHelper{
@@ -10,14 +11,14 @@ namespace ExpenseTracker.Database{
             this.connectionString = connectionString;
         }
 
-    // Example: Add a user
+        // Example: Add a user
         public void AddUser(User user)
         {
-            using (var connection = new MySqlConnection(connectionString))
+            using (var connection = new MySql.Data.MySqlClient.MySqlConnection(connectionString))
             {
                 connection.Open();
                 string query = "INSERT INTO User (Username, PasswordHash, Name, Email, Salt) VALUES (@Username, @PasswordHash, @Name, @Email, @Salt)";
-                using (var cmd = new MySqlCommand(query, connection))
+                using (var cmd = new MySql.Data.MySqlClient.MySqlCommand(query, connection))
                 {
                     cmd.Parameters.AddWithValue("@Username", user.Username);
                     cmd.Parameters.AddWithValue("@PasswordHash", user.PasswordHash);
@@ -30,3 +31,4 @@ namespace ExpenseTracker.Database{
         }
     }
 }
+    
--- ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ec53c23004983bafe69d63415fd4516f181770")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+644d60dc3a6718e85d2bf83f60e803aed195023a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
