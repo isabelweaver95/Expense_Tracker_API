817109b3b5495d03d22ce3af41ef8504ee960868
isabelweaver95 <izzyweav@gmail.com>
2024-10-31 21:57:39 -0600

    Created the Create methods and the user read.

--- ExpenseTracker/Database/DatabaseHelper.cs
+++ ExpenseTracker/Database/DatabaseHelper.cs
@@ -11,7 +11,10 @@ public class DatabaseHelper
         this.connectionString = connectionString;
     }
 
-    // Example: Add a user
+    //Starting to create the CRUD methods
+    // Create Methods
+
+    //This will add a user (create)
     public void AddUser(User user)
     {
         using (var connection = new MySqlConnection(connectionString))
@@ -30,5 +33,66 @@ public class DatabaseHelper
         }
     }
 
-    // Additional methods for Expense CRUD, getting a user's expenses, etc.
+    //This will add an expense (Create)
+    public void AddExpense(Expense expense){
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "INSERT INTO Expense (UserID, Amount, Date, Description, CategoryID) VALUES (@UserID, @Amount, @Date, @Description, @CategoryID)";
+            using (var cmd = new MySqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@UserID", expense.UserID);
+                cmd.Parameters.AddWithValue("@Amount", expense.Amount);
+                cmd.Parameters.AddWithValue("@Date", expense.Date);
+                cmd.Parameters.AddWithValue("@Description", expense.Description);
+                cmd.Parameters.AddWithValue("@CategoryID", expense.CatagoryId);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    //This will add a category (Create)
+    public void AddCatagory(Catagory catagory){
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "INSERT INTO Catagory (Name) VALUES (@Name)";
+            using (var cmd = new MySqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@Name", catagory.Name);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+
+    // Read Methods
+
+    //This will get all users (read)
+    public List<User> GetAllUsers(){
+        List<User> users = new List<User>();
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "SELECT * FROM User";
+            using (var cmd = new MySqlCommand(query, connection))
+            {
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32("Id");
+                        string username = reader.GetString("Username");
+                        string name = reader.GetString("Name");
+                        string email = reader.GetString("Email");
+                        string passwordHash = reader.GetString("PasswordHash");
+                        string salt = reader.GetString("Salt");
+                    }
+                }
+            }
+        }
+        return users;
+    }
+
+
 }
