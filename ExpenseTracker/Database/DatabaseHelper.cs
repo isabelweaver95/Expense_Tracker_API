89875152bd4b940f617618147e491c779c0a525c
isabelweaver95 <izzyweav@gmail.com>
2024-10-31 21:25:10 -0600

    saved

--- ExpenseTracker/Database/DatabaseHelper.cs
+++ ExpenseTracker/Database/DatabaseHelper.cs
@@ -1,34 +1,34 @@
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
 using ExpenseTracker.Models;
-using MySqlConnector;
 
+public class DatabaseHelper
+{
+    private readonly string connectionString;
 
-namespace ExpenseTracker.Database{
-    public class DatabaseHelper{
-        private readonly string connectionString;
+    public DatabaseHelper(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
 
-        public DatabaseHelper(string connectionString)
+    // Example: Add a user
+    public void AddUser(User user)
+    {
+        using (var connection = new MySqlConnection(connectionString))
         {
-            this.connectionString = connectionString;
-        }
-
-        // Example: Add a user
-        public void AddUser(User user)
-        {
-            using (var connection = new MySql.Data.MySqlClient.MySqlConnection(connectionString))
+            connection.Open();
+            string query = "INSERT INTO User (Username, PasswordHash, Name, Email, Salt) VALUES (@Username, @PasswordHash, @Name, @Email, @Salt)";
+            using (var cmd = new MySqlCommand(query, connection))
             {
-                connection.Open();
-                string query = "INSERT INTO User (Username, PasswordHash, Name, Email, Salt) VALUES (@Username, @PasswordHash, @Name, @Email, @Salt)";
-                using (var cmd = new MySql.Data.MySqlClient.MySqlCommand(query, connection))
-                {
-                    cmd.Parameters.AddWithValue("@Username", user.Username);
-                    cmd.Parameters.AddWithValue("@PasswordHash", user.PasswordHash);
-                    cmd.Parameters.AddWithValue("@Name", user.Name);
-                    cmd.Parameters.AddWithValue("@Email", user.Email);
-                    cmd.Parameters.AddWithValue("@Salt", user.Salt);
-                    cmd.ExecuteNonQuery();
-                }
+                cmd.Parameters.AddWithValue("@Username", user.Username);
+                cmd.Parameters.AddWithValue("@PasswordHash", user.PasswordHash);
+                cmd.Parameters.AddWithValue("@Name", user.Name);
+                cmd.Parameters.AddWithValue("@Email", user.Email);
+                cmd.Parameters.AddWithValue("@Salt", user.Salt);
+                cmd.ExecuteNonQuery();
             }
         }
     }
+
+    // Additional methods for Expense CRUD, getting a user's expenses, etc.
 }
-    
